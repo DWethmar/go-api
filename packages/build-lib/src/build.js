88b3ee569e3b73fbf7ce44a06d5dc4367f729142
Dennis Wethmar <denniswethmar@gmail.com>
2020-04-15 13:47:44 +0200

    Workable ui setup

--- packages/build-lib/src/build.js
+++ packages/build-lib/src/build.js
@@ -1,76 +1,59 @@
 #!/usr/bin/env node
 
-const rollup = require('rollup');
-const rollupTypescript = require('@rollup/plugin-typescript');
+const rollup = require("rollup");
+const rollupTypescript = require("rollup-plugin-typescript2");
+const __typescript = require("typescript");
+const sass = require('rollup-plugin-sass')
+
+const commonjs = require("@rollup/plugin-commonjs");
+const resolve = require("@rollup/plugin-node-resolve");
+const excludeDependenciesFromBundle = require("rollup-plugin-exclude-dependencies-from-bundle");
 
 // see below for details on the options
 const inputOptions = {
-    input: 'src/index.ts',
-    plugins: [
-      rollupTypescript()
-    ]
+  input: "src/index.ts",
+  plugins: [
+    excludeDependenciesFromBundle(),
+    rollupTypescript({
+      tsconfig: "tsconfig.json",
+      typescript: __typescript,
+    }),
+    sass({
+      output: true,
+    }),
+    resolve(),
+    commonjs(),
+  ],
 };
 
 const outputOptions = [
-    {
-      file: 'lib/bundle-b1.js',
-      format: 'cjs'
-    },
-    {
-      file: 'lib/bundle-b2.js',
-      format: 'es'
-    }
+  {
+    file: "./lib/index.js",
+    format: "cjs",
+  },
+  {
+    file: "./lib/index.es.js",
+    format: "es",
+  },
 ];
 
+const generateBundles = (bundle) =>
+  Promise.all(
+    outputOptions.map((outputOption) => {
+      console.log(`Writing ${outputOption.file}`);
+      return bundle.write(outputOption);
+    })
+  ).then(() => bundle);
 
 async function build() {
   // create a bundle
-  const bundle = await rollup.rollup(inputOptions);
-
-  console.log(bundle.watchFiles); // an array of file names this bundle depends on
-
-  // generate output specific code in-memory
-  // you can call this function multiple times on the same bundle object
-  const { output } = await bundle.generate(outputOptions);
-
-  for (const chunkOrAsset of output) {
-    if (chunkOrAsset.type === 'asset') {
-      // For assets, this contains
-      // {
-      //   fileName: string,              // the asset file name
-      //   source: string | Uint8Array    // the asset source
-      //   type: 'asset'                  // signifies that this is an asset
-      // }
-      console.log('Asset', chunkOrAsset);
-    } else {
-      // For chunks, this contains
-      // {
-      //   code: string,                  // the generated JS code
-      //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-      //   exports: string[],             // exported variable names
-      //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-      //   fileName: string,              // the chunk file name
-      //   imports: string[],             // external modules imported statically by the chunk
-      //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-      //   isEntry: boolean,              // is this chunk a static entry point
-      //   map: string | null,            // sourcemaps if present
-      //   modules: {                     // information about the modules in this chunk
-      //     [id: string]: {
-      //       renderedExports: string[]; // exported variable names that were included
-      //       removedExports: string[];  // exported variable names that were removed
-      //       renderedLength: number;    // the length of the remaining code in this module
-      //       originalLength: number;    // the original length of the code in this module
-      //     };
-      //   },
-      //   name: string                   // the name of this chunk as used in naming patterns
-      //   type: 'chunk',                 // signifies that this is a chunk
-      // }
-      console.log('Chunk', chunkOrAsset.modules);
-    }
-  }
-
-  // or write the bundle to disk
-  await bundle.write(outputOptions);
+  const bundle = await rollup
+    .rollup(inputOptions)
+    .then(generateBundles)
+    .catch((error) => {
+      console.error(error);
+      return process.exit(-1);
+    });
 }
 
 build();
